0faf8a569f889e61226b136e31060ef957e743a1
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-06-13 20:07:58 +0100

    hide line numbers and gutter signs/icons in inactive windows

--- init.lua
+++ init.lua
@@ -274,6 +274,26 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- Hide line numbers and gutter signs/icons in inactive windows
+-- See https://superuser.com/questions/385553/making-the-active-window-in-vim-more-obvious
+local bghighlight_group = vim.api.nvim_create_augroup('BgHighlight', {})
+vim.api.nvim_create_autocmd('WinEnter', {
+  group = bghighlight_group,
+  command = "set number",
+})
+vim.api.nvim_create_autocmd('WinEnter', {
+  group = bghighlight_group,
+  command = "set signcolumn=yes",
+})
+vim.api.nvim_create_autocmd('WinLeave', {
+  group = bghighlight_group,
+  command = "set nonumber",
+})
+vim.api.nvim_create_autocmd('WinLeave', {
+  group = bghighlight_group,
+  command = "set signcolumn=no",
+})
+
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
