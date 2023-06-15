e1d8b7169598cea3a33246a4b7c5cffb2026652b
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-06-15 20:35:22 +0100

    `help` was renamed to `vimdoc`
    see https://github.com/LazyVim/LazyVim/issues/524#issuecomment-1493406470

--- init.lua
+++ init.lua
@@ -332,7 +332,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vimdoc', 'vim', 'beancount', 'dart' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'beancount', 'dart' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
