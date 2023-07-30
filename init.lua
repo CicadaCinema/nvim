6550243e86da9ea8361b1620120e13b467cb6857
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-07-30 21:09:45 +0100

    set the lualine colour scheme to dracula

--- init.lua
+++ init.lua
@@ -145,7 +145,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'dracula',
         component_separators = '|',
         section_separators = '',
       },
