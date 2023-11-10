b77f7eabfb979a357aec8ba47f3ddf353e28e0e8
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-11-10 21:15:28 +0000

    ARCHIVE: dont know what this is supposed to do, since wrapping works fine without this line

--- init.lua
+++ init.lua
@@ -244,6 +244,7 @@ vim.wo.signcolumn = 'yes'
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
+vim.opt.wrap = true
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
