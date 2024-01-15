e260db7f85149d6de64464813f461d5c081cab85
Arseny <git@arseny.uk>
2024-01-15 22:10:25 +0000

    revert the lualine colour scheme to the original onedark
    this reverts commit a87124ed6af0cce8f0769203d20f04a537a4a109

--- init.lua
+++ init.lua
@@ -168,7 +168,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'dracula',
+        theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
